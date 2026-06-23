b276594822575ac419b76641387ab54350c65e0c
Arsath Suhail <rafiibrahim818@gmail.com>
2026-06-24 05:16:36 +0530

    start And bulid

--- server/app.ts
+++ server/app.ts
@@ -32,6 +32,8 @@ app.post("/api/bookings", async (req : any, res : any) => {
   });
 });
 
-app.listen(5000, () =>{
-    console.log('Server is running on port 5000');
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
